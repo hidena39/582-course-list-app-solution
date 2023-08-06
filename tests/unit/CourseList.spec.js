4813d32506f122820d4cc44f794d7d2dcac2e9f9
DIDI-LAPTOP\hiden <hidenadeluca@gmail.com>
2023-08-05 20:07:02 -0400

    finished

--- tests/unit/CourseList.spec.js
+++ tests/unit/CourseList.spec.js
@@ -1,7 +1,18 @@
 import { shallowMount } from "@vue/test-utils";
 import CourseList from "@/components/CourseList.vue";
+import CourseItem from "@/components/CourseItem.vue";
 
 describe("CourseList.vue", () => {
-  it("method/emitted", () => {});
-  it("method/received", () => {});
+  it("emits addCourse event when Add Course button is clicked in CourseItem.vue", async () => {
+    const courses = [
+      {
+        id: 8,
+      },
+    ];
+    const wrapper = shallowMount(CourseList, {
+      props: { courses },
+    });
+    await wrapper.findComponent(CourseItem).vm.$emit("addCourse", 8);
+    expect(wrapper.emitted().addCourse[0][0]).toBe(8);
+  });
 });
