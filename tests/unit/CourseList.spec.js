cb7c4043bca438394315aa55d25afa3ad6cecebc
DIDI-LAPTOP\hiden <hidenadeluca@gmail.com>
2023-08-06 12:33:30 -0400

    added remove course test on CourseList.spec.js

--- tests/unit/CourseList.spec.js
+++ tests/unit/CourseList.spec.js
@@ -15,4 +15,16 @@ describe("CourseList.vue", () => {
     await wrapper.findComponent(CourseItem).vm.$emit("addCourse", 8);
     expect(wrapper.emitted().addCourse[0][0]).toBe(8);
   });
+  it("emits removeCourse event when Remove Course button is clicked in CourseItem.vue", async () => {
+    const courses = [
+      {
+        id: 2,
+      },
+    ];
+    const wrapper = shallowMount(CourseList, {
+      props: { courses },
+    });
+    await wrapper.findComponent(CourseItem).vm.$emit("removeCourse", 8);
+    expect(wrapper.emitted().removeCourse[0][0]).toBe(8);
+  });
 });
